e5874e64405a88c7cf98a4890ea3859a1cf017a3
Ole994 <ole.korvald1994@gmail.com>
2021-02-01 20:52:17 +0100

    Ma1-Js

--- js/script.js
+++ js/script.js
@@ -1,14 +1,24 @@
-const cats = [
-    {
-        name: "Blob",
-        age: 10
-    },
-    {
-        name: "Harold",
-    },
-    {
-        name: "Blurt",
-        age: 21
-    }
-];
+//Question 1:
+let cat = {
+};
+cat.complain = function(){
+	alert("Meow!")
+}
+cat.complain();
 
+// Question 2:
+const heading = document.querySelector ("h3")
+heading.innerHTML = "uptaded heading";
+
+// Question 3:
+heading.style.fontSize = "2em";
+
+
+// Question 4:
+const newClass = document.querySelector ("h3");
+newClass.classList.add("subheading");
+
+//Question 5
+let paragraphs = document.querySelectorAll ("p");
+paragraphs.forEach 
+(function (p) {p.style.color = "red"})
