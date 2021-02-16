fd8e52b1a64c7e059c7727b15f218131d8d0305d
Ole994 <ole.korvald1994@gmail.com>
2021-02-16 13:06:06 +0100

    i

--- js/script.js
+++ js/script.js
@@ -1,10 +1,13 @@
 //Question 1:
-let cat = {
-};
-cat.complain = function(){
-	alert("Meow!")
+let 
+	cat = {
+	complain: function ()
+	{
+		console.log ("meow!");
+	}
 }
-cat.complain();
+cat.complain ()
+
 
 // Question 2:
 const heading = document.querySelector ("h3")
@@ -21,4 +24,13 @@ newClass.classList.add("subheading");
 //Question 5
 let paragraphs = document.querySelectorAll ("p");
 paragraphs.forEach 
-(function (p) {p.style.color = "red"})
+(function (p) {p.style.color = "red"});
+
+let b = document.querySelector("button")
+b.style.fontSize = "2em";
+
+// Exercise 4
+// // Add an eventListener to the button (with id "tBtn") 
+// and make a function to toggle the class makeMeRed on and off for the footer element.
+
+
